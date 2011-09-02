aa5e32d5fae31eeba753958ca10f777ac3551f5e
Tom Brennan <tommie.brennan@gmail.com>
2011-09-02 15:41:50 +0100

    Added .config

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Fri Sep 2 14:32:49 IST 2011"
-#define LINUX_COMPILE_TIME "14:32:49"
+#define UTS_VERSION "#2 PREEMPT Fri Sep 2 15:40:55 IST 2011"
+#define LINUX_COMPILE_TIME "15:40:55"
 #define LINUX_COMPILE_BY "t0mm13b"
 #define LINUX_COMPILE_HOST "tribbles"
 #define LINUX_COMPILE_DOMAIN "(none)"
