11ce00c33bee8d248161984ea25b819ddcc38705
Phoenix-Silver <ftpb@free.fr>
2011-09-02 11:15:25 +0200

    Added blade files

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 0 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 PREEMPT Fri Sep 2 09:42:19 CEST 2011"
+#define LINUX_COMPILE_TIME "09:42:19"
+#define LINUX_COMPILE_BY "phoenix"
+#define LINUX_COMPILE_HOST "Phoenix"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
