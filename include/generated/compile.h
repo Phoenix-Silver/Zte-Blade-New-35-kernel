ea78e96b3431a6b395039e390a26e749390055b6
Tom Brennan <tommie.brennan@gmail.com>
2011-09-02 15:38:53 +0100

    Added gsf config, copied blade2 board into keypad and blade to satisfy gsf config, removed the compile warning flag

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,9 +1,9 @@
 /* This file is auto generated, version 1 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Fri Sep 2 11:42:26 IST 2011"
-#define LINUX_COMPILE_TIME "11:42:26"
+#define UTS_VERSION "#1 PREEMPT Fri Sep 2 14:32:49 IST 2011"
+#define LINUX_COMPILE_TIME "14:32:49"
 #define LINUX_COMPILE_BY "t0mm13b"
 #define LINUX_COMPILE_HOST "tribbles"
 #define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
